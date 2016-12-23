e7e6928cb8a0439ea5cb59ae35bf6745e60b62ba
Jayden <mischievousjayden@gmail.com>
2016-12-23 02:16:43 -0500

    added install zfs script

---
+++ instruction/script/install_zfs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "----- START TO INSTALL ZFS -----"
+echo "----- add repository to pacman.conf -----"
+PACMAN_CONF=/etc/pacman.conf
+echo "" >> $PACMAN_CONF
+echo "[archzfs]" >> $PACMAN_CONF
+echo "Server = http://archzfs.com/\$repo/x86_64" >> $PACMAN_CONF
+echo "" >> $PACMAN_CONF
+
+echo "----- add repository key -----"
+pacman-key -r 5E1ABF240EE7A126
+pacman-key --lsign-key 5E1ABF240EE7A126
+
+echo "----- install zfs -----"
+pacman -Syyu
+pacman -S zfs-linux
+
